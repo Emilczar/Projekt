fe245eeda14a24d8160a07bbbd1ce2707284e24c
Emilczar <emilczar@gmail.com>
2015-03-22 10:40:33 +0100

    kolejne

--- openMP/1.cpp
+++ openMP/1.cpp
@@ -1,90 +1,29 @@
-
-#include <iostream>
-#include <string>
-#include <conio.h>
+#include <cstdio>
 #include <time.h>
 #include <omp.h>
 
-void licz()
+int main()
 {
-	char wzorzec[] = "abc";
-	char tekst[] =  "abcabcabc" ;
-	int m, n, i, j, ilosc;
-	ilosc = 0;
-	n = strlen(tekst);
-	m = strlen(wzorzec);
+	clock_t start, stop;
+	int n =100000000;
+	bool p;
+	int i ;
 	
-	i = 0;
-	while (i <= n - m)
-	{
-		j = 0;
-		while ((j<m) && (wzorzec[j] == tekst[i + j])) j++;
-		if (j == m) ilosc = ilosc + 1;
-		
-		i++;
-	}
-	printf("wystapienie klucza: %d \n", ilosc);
-
-}
-
-int main(void)
-{
-	clock_t start, koniec, start1, koniec1;	start = clock();
-	licz();
-
-	koniec = clock();
-	long delta = (long)(koniec - start);
-	printf("Czas wykonywania obliczen : %d ms \n", koniec);
-
-	start1 = clock();
-#pragma omp parallel sections
-	{
-#pragma omp section
+	start = clock();
+#pragma omp parallel for
+		for (i = 1; i < n ; i++)
 		{
-			licz();
-			licz();
-			licz();
-			licz();
-			printf("policzone %d.\n", omp_get_thread_num());
+			p = true;
+
+			for (int j = 2; j < i; j++)
+		if (i % j == 0)
+				p = false;
+			if (p == true)
+				printf("%d\n", i);
+
+
 		}
-
-#pragma omp section
-		{
-		licz();
-		licz();
-		licz();
-		licz();
-			printf("policzone %d.\n", omp_get_thread_num());
-	}
-#pragma omp section
-		{
-			licz();
-			licz();
-			licz();
-			licz();
-			printf("policzone %d.\n", omp_get_thread_num());
-	}
-#pragma omp section
-		{
-			licz();
-			licz();
-			licz();
-			licz();
-			printf("policzone %d.\n", omp_get_thread_num());
-		}
-		#pragma omp section
-		{
-			licz();
-			licz();
-			licz();
-			licz();
-			printf("policzone %d.\n", omp_get_thread_num());
-		}
-
-	}
-	koniec1 = clock();
-	long delta1 = (long)(koniec1 - start1);
-	printf("Czas wykonywania obliczen: %d ms ", delta1);
 	
-	getchar();
+	stop = clock();
+	printf("Czas przetwarzania wynosi %f sekund\n",((double)(stop - start)/1000.0));
 }
